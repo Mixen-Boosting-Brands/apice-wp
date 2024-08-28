a6f9437a9c2eb485dafd5dbac4833b40792b430f
Luis Carlos Pando <hey@luiscarlospando.com>
2024-08-28 11:20:48 -0600

    Bundle para producción

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-    entry: './src/app.js',
+    entry: "./src/app.js",
     output: {
-        filename: 'app.bundle.js',
-        path: path.resolve(__dirname, 'assets/js')
+        filename: "app.bundle.js",
+        path: path.resolve(__dirname, "assets/js"),
     },
-    mode: 'development',
+    mode: "production",
     module: {
         rules: [
             {
@@ -17,19 +17,19 @@ module.exports = {
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+                        presets: ["@babel/preset-env"],
+                    },
+                },
             },
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
+                use: ["style-loader", "css-loader"],
+            },
+        ],
     },
     devServer: {
         static: {
-          directory: path.join(__dirname, '/'),
+            directory: path.join(__dirname, "/"),
         },
         compress: true,
         port: 7777,
@@ -37,7 +37,7 @@ module.exports = {
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
-            jQuery: "jquery"
-        })
-    ]
-}
+            jQuery: "jquery",
+        }),
+    ],
+};
