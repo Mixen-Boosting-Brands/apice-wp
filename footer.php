58fd5a4890c1bb7194f790569bc7543a277f730e
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-11 16:46:29 -0600

    Ir armando carrusel internas

--- footer.php
+++ footer.php
@@ -1089,6 +1089,6 @@
 
         <script src="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/js/app.bundle.js?v=2"></script>
+        ); ?>/assets/js/app.bundle.js?v=3"></script>
     </body>
 </html>
