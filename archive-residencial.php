9c43667957888e42a69c3c0a09df21821554ba88
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-11 16:40:52 -0600

    Ir armando carrusel internas

--- archive-residencial.php
+++ archive-residencial.php
@@ -1,54 +1,5 @@
 <?php get_header(); ?>
 
-<section id="jumbotron">
-    <div class="overlay"></div>
-    <div class="swiper swiper-internas">
-        <div class="swiper-wrapper">
-            <div
-                class="swiper-slide"
-                style="
-                    background: url(&quot;<?php echo esc_url(
-                        get_template_directory_uri()
-                    ); ?>/assets/images/residencial/thumb-modelo-1.webp&quot;)
-                        no-repeat;
-                "
-            >
-                <div class="container-fluid">
-                    <div class="row">
-                        <div
-                            class="col d-flex justify-content-start align-items-end"
-                        >
-                            <h1
-                                class="titulo ms-lg-4 mb-5"
-                                data-aos="fade-up"
-                                data-aos-duration="1000"
-                            >
-                                Proyectos Residenciales
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- If we need pagination -->
-        <div class="swiper-pagination"></div>
-
-        <!-- If we need navigation buttons -->
-        <!-- div class="swiper-button-next"></div -->
-        <!-- div class="swiper-button-prev"></div -->
-
-        <!-- If we need scrollbar -->
-        <!-- div class="swiper-scrollbar"></div>
-        <div class="autoplay-progress">
-            <svg viewBox="0 0 48 48">
-                <circle cx="24" cy="24" r="20"></circle>
-            </svg>
-            <span></span>
-        </div -->
-    </div>
-    <div class="flash"></div>
-</section>
-
 <?php
 // Define a custom query to fetch all posts from 'residencial' post type
 $residencial_query = new WP_Query([
@@ -69,6 +20,7 @@ if ($residencial_query->have_posts()): ?>
                 <?php // Loop through all posts from the custom query
     // Loop through all posts from the custom query
     // Loop through all posts from the custom query
+                // Loop through all posts from the custom query
                 while ($residencial_query->have_posts()):
                     $residencial_query->the_post(); ?>
                     <div class="swiper-slide">
@@ -99,6 +51,7 @@ if ($residencial_query->have_posts()): ?>
 <?php // Reset post data after the custom query loop
     // Reset post data after the custom query loop
     // Reset post data after the custom query loop
+// Reset post data after the custom query loop
 wp_reset_postdata();else: ?>
     <p>No posts found.</p>
 <?php endif;
