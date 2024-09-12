adf6245435988842e9911f0cd639b3c4201f2f6c
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-12 12:14:52 -0600

    Armar header de singles

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
         the_post(); ?>
 
 <section id="interna" class="black-headings pt-60 pb-30">
-    <div class="container mb-5">
+    <div class="container custom-container mb-5">
         <div class="bg-post-details row g-0">
             <div class="col-lg-6 my-auto">
                 <div
