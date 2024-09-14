4cffde45e52af7db40b7a0d190c7952e2182f735
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-14 16:19:56 -0600

    Resulta que no hay necesidad de hackear el archive original, restaurar

--- archive.php
+++ archive.php
@@ -1,57 +1,55 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()): ?>
-
-<section id="interna" class="black-headings pt-100 pb-30">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h1
-                    class="titulo ms-lg-4 mb-5"
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                >
-                    Proyectos División Industrial
-                </h1>
+<section id="jumbotron">
+    <div class="overlay"></div>
+    <div class="swiper swiper-internas">
+        <div class="swiper-wrapper">
+            <div
+                class="swiper-slide"
+                style="
+                    background: url(&quot;<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/header-general/bg.webp&quot;)
+                        no-repeat;
+                "
+            >
+                <div class="container-fluid">
+                    <div class="row">
+                        <div
+                            class="col d-flex justify-content-start align-items-end"
+                        >
+                            <h1
+                                class="titulo ms-lg-4 mb-5"
+                                data-aos="fade-up"
+                                data-aos-duration="1000"
+                            >
+                                Archivos
+                            </h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div
-        class="swiper swiper-proyectos"
-        data-aos="fade-in"
-        data-aos-duration="1000"
-        data-aos-delay="100"
-    >
-        <div class="swiper-wrapper">
-            <?php while (have_posts()):
-                the_post(); ?>
-                <div class="swiper-slide">
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_post_thumbnail("proyecto", [
-                            "class" => "img-fluid mb-3",
-                        ]); ?>
-                    </a>
-                    <a href="<?php the_permalink(); ?>">
-                        <h2>
-                            <?php the_title(); ?>
-                        </h2>
-                    </a>
-                </div>
-            <?php
-            endwhile; ?>
-        </div>
-
-        <!-- Navigation buttons for Swiper -->
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-
         <!-- If we need pagination -->
-        <div class="swiper-pagination-proyectos"></div>
+        <div class="swiper-pagination"></div>
+
+        <!-- If we need navigation buttons -->
+        <!-- div class="swiper-button-next"></div -->
+        <!-- div class="swiper-button-prev"></div -->
 
         <!-- If we need scrollbar -->
-        <div class="swiper-scrollbar-proyectos mt-4"></div>
+        <!-- div class="swiper-scrollbar"></div>
+        <div class="autoplay-progress">
+            <svg viewBox="0 0 48 48">
+                <circle cx="24" cy="24" r="20"></circle>
+            </svg>
+            <span></span>
+        </div -->
     </div>
+    <div class="flash"></div>
 </section>
+
+<?php if (have_posts()): ?>
 
 <section id="interna" class="black-headings pt-60 pb-30">
     <div class="container">
