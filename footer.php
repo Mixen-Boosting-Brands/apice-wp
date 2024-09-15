5cea8dfd88c93a49f10a32680d153e018086d5d5
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-14 19:33:49 -0600

    Vamos acomodando las tranzas

--- footer.php
+++ footer.php
@@ -1089,6 +1089,6 @@
 
         <script src="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/js/app.bundle.js?v=13"></script>
+        ); ?>/assets/js/app.bundle.js?v=14"></script>
     </body>
 </html>
