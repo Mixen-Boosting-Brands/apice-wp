96f87d6d6db86480476db32bfff483fb9ad2fd33
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-17 15:10:35 -0600

    Ir armando carrusel vertical

--- single-multimedia.php
+++ single-multimedia.php
@@ -4,7 +4,7 @@
     while (have_posts()):
         the_post(); ?>
 
-<section id="interna" class="interna-dark interna-multimedia pt-60 pb-30">
+<section id="interna" class="interna-dark interna-multimedia single-multimedia p-100 pb-30">
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
