2aa5f0a9f609c6d3dc4db1321ce3c4c5943088af
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-17 15:27:14 -0600

    Ir armando carrusel vertical

--- header.php
+++ header.php
@@ -54,7 +54,7 @@
 
         <link rel="stylesheet" href="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/css/styles.css?v=50" />
+        ); ?>/assets/css/styles.css?v=51" />
 
         <?php wp_head(); ?>
     </head>
