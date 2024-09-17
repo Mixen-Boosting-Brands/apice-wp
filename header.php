66ae1b7c386d81ad6e35bd3dd70b488020234fa2
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-16 19:52:05 -0600

    Vamos acomodando las tranzas

--- header.php
+++ header.php
@@ -54,7 +54,7 @@
 
         <link rel="stylesheet" href="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/css/styles.css?v=26" />
+        ); ?>/assets/css/styles.css?v=27" />
 
         <?php wp_head(); ?>
     </head>
