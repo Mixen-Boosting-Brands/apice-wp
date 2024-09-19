06dc2da9cb01f0c5b88df8a46ad12c69bf3de89a
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-19 12:40:58 -0600

    Limpiar caché

--- footer.php
+++ footer.php
@@ -1129,6 +1129,6 @@
 
         <script src="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/js/app.bundle.js?v=63"></script>
+        ); ?>/assets/js/app.bundle.js?v=64"></script>
     </body>
 </html>
