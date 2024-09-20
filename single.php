29fbc043e88699846437f1ae625d19737f6aa0d8
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-20 16:37:22 -0600

    Ocultar header si el post mostrado es descargable

--- single.php
+++ single.php
@@ -5,82 +5,86 @@
         the_post(); ?>
 
 <section id="interna" class="black-headings pt-100 pb-30">
-    <div class="container custom-container mb-5">
-        <div class="bg-post-details row g-0">
-            <div class="col-lg-6 my-auto">
-                <div
-                    class="post-thumbnail"
-                    data-aos="fade-in"
-                    data-aos-duration="1000"
-                    style="
-                        background: url(&quot;<?php echo get_the_post_thumbnail_url(
-                            get_the_ID(),
-                            "jumbotron"
-                        ); ?>&quot;)
-                            no-repeat;
-                    "
-                >
+    <?php if (!is_singular("descargables")): ?>
+        <div class="container custom-container mb-5">
+            <div class="bg-post-details row g-0">
+                <div class="col-lg-6 my-auto">
+                    <div
+                        class="post-thumbnail"
+                        data-aos="fade-in"
+                        data-aos-duration="1000"
+                        style="
+                            background: url(&quot;<?php echo get_the_post_thumbnail_url(
+                                get_the_ID(),
+                                "jumbotron"
+                            ); ?>&quot;)
+                                no-repeat;
+                        "
+                    >
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6 my-auto">
-                <div class="post-details">
-                    <h6
-                        data-aos="fade-up"
-                        data-aos-duration="1000"
-                    >
-                        <time>
-                            <?php
-                            // Set the date and time format
-                            $date_format = 'j \d\e F, Y \· g:i a';
-
-                            // Replace the problematic character sequence manually
-                            $formatted_date = date_i18n(
-                                $date_format,
-                                strtotime(get_the_date("Y-m-d H:i:s"))
-                            );
-
-                            // Output the final formatted date and time
-                            echo "" . $formatted_date;
-                            ?>
-        				</time>
-                    </h6>
-
-                    <h1
-                        class="my-5"
-                        data-aos="fade-up"
-                        data-aos-duration="1000"
-                        data-aos-delay="200"
-                    >
-                        <?php the_title(); ?>
-                    </h1>
-
-                    <?php if (get_field("entradilla")): ?>
-                        <div
-                            class="text-start mb-5"
+                <div class="col-lg-6 my-auto">
+                    <div class="post-details">
+                        <h6
                             data-aos="fade-up"
                             data-aos-duration="1000"
-                            data-aos-delay="400"
                         >
-                            <p><?php the_field("entradilla"); ?></p>
-                        </div>
-                    <?php endif; ?>
+                            <time>
+                                <?php
+                                // Set the date and time format
+                                $date_format = 'j \d\e F, Y \· g:i a';
 
-                    <h6
-                        data-aos="fade-up"
-                        data-aos-duration="1000"
-                        data-aos-delay="600"
-                    >
-                        <?php esc_html_e(
-                            "Por",
-                            "html5blank"
-                        ); ?> <?php echo get_the_author_meta("user_firstname") .
+                                // Replace the problematic character sequence manually
+                                $formatted_date = date_i18n(
+                                    $date_format,
+                                    strtotime(get_the_date("Y-m-d H:i:s"))
+                                );
+
+                                // Output the final formatted date and time
+                                echo "" . $formatted_date;
+                                ?>
+            				</time>
+                        </h6>
+
+                        <h1
+                            class="my-5"
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="200"
+                        >
+                            <?php the_title(); ?>
+                        </h1>
+
+                        <?php if (get_field("entradilla")): ?>
+                            <div
+                                class="text-start mb-5"
+                                data-aos="fade-up"
+                                data-aos-duration="1000"
+                                data-aos-delay="400"
+                            >
+                                <p><?php the_field("entradilla"); ?></p>
+                            </div>
+                        <?php endif; ?>
+
+                        <h6
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="600"
+                        >
+                            <?php esc_html_e(
+                                "Por",
+                                "html5blank"
+                            ); ?> <?php echo get_the_author_meta(
+     "user_firstname"
+ ) .
      " " .
      get_the_author_meta("user_lastname"); ?>
-                    </h6>
+                        </h6>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    <?php endif; ?>
     <div class="container">
         <div class="row mb-4">
             <div class="col">
